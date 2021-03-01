e709656c1018c73b154e6869298ad6965ce3b9c2
Azhari Subroto <juarathemes@gmail.com>
2021-03-01 08:36:42 +0700

    Button component styling fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,34 +1,30 @@
 import PropTypes from 'prop-types'
-const Button = ({ children, color, dark, width, height, fontSize, ...other }) => {
-  const clr = color ? color : '#ececec'
+import styles from './Button.module.scss'
+
+const Button = ({ children, color, dark, width, height, fontSize, className, ...other }) => {
+  let classes = styles.Button
+  if (className) {
+    classes += ` ${className}`
+  }
+  if (color) {
+    classes += ` ${color}`
+  }
   return (
     <>
-      <button className={color} {...other}>
+      <button
+        className={classes}
+        {...other}
+        style={{
+          width: `${width ? `${width}px` : 'auto'}`,
+          height: `${height ? `${height}px` : 'auto'}`,
+          lineHeight: `${height ? `${height}px` : '1'}`,
+          padding: `${!width ? '8px 13px' : '0'}`,
+          fontSize: `${fontSize ? fontSize : 12}px`,
+          backgroundColor: `${color ? color : '#ececec'}`,
+          color: `${dark ? '#fff' : '#323a44'}`
+        }}>
         {children}
       </button>
-      <style jsx>{`
-        button {
-          width: ${width ? `${width}px` : 'auto'};
-          height: ${height ? `${height}px` : 'auto'};
-          line-height: ${height ? `${height}px` : '1'};
-          padding: ${!width ? '8px 13px' : '0'};
-          font-size: ${fontSize ? fontSize : 12}px;
-          display: flex;
-          border-radius: 6px;
-          background-color: ${clr};
-          color: ${dark ? '#fff' : '#323a44'};
-          border: 0;
-          align-items: center;
-          justify-content: center;
-          cursor: pointer;
-          transition: all 0.1s ease;
-          outline-color: #d4eeea;
-
-          &:hover {
-            filter: brightness(104%);
-          }
-        }
-      `}</style>
     </>
   )
 }
